8f9085d277bb241fdc5194e2d615f7e015591fae
mx <1794824640@qq.com>
2021-09-16 20:42:05 +0800

    项目基础环境创建

---
+++ src/main/java/com/baizhi/Git2106TestApplication.java
@@ -0,0 +1,12 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git2106TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git2106TestApplication.class, args);
+    }
+}
